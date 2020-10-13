b4fae35aebed7aea04da14bb0a9e53e37993add9
AdairHdz <adairho16@gmail.com>
2020-10-13 09:34:21 -0500

    Se creó carpeta Langs para mover los resources

--- MemoryGame/Properties/Langs/Resources.Designer.cs
+++ MemoryGame/Properties/Langs/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MemoryGame.Properties {
+namespace MemoryGame.Properties.Langs {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MemoryGame.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MemoryGame.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MemoryGame.Properties.Langs.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
