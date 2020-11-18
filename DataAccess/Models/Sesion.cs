39bef10a0415449636b44ee991eab41b48cd3966
Baltix2k <Baltix2k@users.noreply.github.com>
2020-11-17 22:37:26 -0600

    Agregacion de chat

---
+++ DataAccess/Models/Sesion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Models
+{
+    public class Sesion
+    {
+        private static Sesion playerSesion = null;
+        public string EmailAddress { get; set; }
+        public string Username { get; set; }
+
+        private Sesion() { }
+        public static Sesion getSesionData
+        {
+            get
+            {
+                if (playerSesion == null)
+                {
+                    playerSesion = new Sesion();
+                }
+                return playerSesion;
+            }
+        }
+    }
+}
--- MemoryGame/Views/Chat.xaml.cs
+++ MemoryGame/Views/Chat.xaml.cs
@@ -12,17 +12,55 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ServiceModel;
+using DataAccess.Models;
 
-namespace MemoryGame.Views
+namespace MemoryGame
 {
     /// <summary>
     /// Lógica de interacción para Chat.xaml
     /// </summary>
-    public partial class Chat : Page
+    public partial class Chat : Window //Proxy.ICommunicationServiceCallback
     {
+       /* private Proxy.CommunicationServiceClient server = null;
+        private InstanceContext context = null;
+        Sesion playerSesion = Sesion.getSesionData;
         public Chat()
         {
             InitializeComponent();
+            context = new InstanceContext(this);
+            server = new Proxy.CommunicationServiceClient(context);
+            server.Join(playerSesion.Username);
+
         }
+
+        public void ReciveMessage(string username, string message)
+        {
+            string format = "\n" + username + ": " + message;
+            ChatBox.AppendText(format);
+            ChatBox.ScrollToEnd();
+        }
+
+        private void SendMessageClickedButton(object sender, RoutedEventArgs e)
+        {
+            string message = TextBoxMessageContent.Text;
+            if (!string.IsNullOrEmpty(message))
+            {
+                server.SendMessage(message);
+            }
+
+            string format = "\nTu: " + message;
+            ChatBox.AppendText(format);
+            ChatBox.ScrollToEnd();
+            TextBoxMessageContent.Clear();
+        }
+
+        /*private void EnterKeyPressed (object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendMessageClickedButton(this, new RoutedEventArgs());
+            }
+        }*/
     }
 }
