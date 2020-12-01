b0deaf1b808fe5edefb56e0f6bf92f77ce153947
AdairHdz <adairho16@gmail.com>
2020-11-30 23:17:08 -0600

    Antes de copiar algoritmo de revolver cartas

---
+++ MemoryGameService/DataTransferObjects/CardDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryGame.MemoryGameService.DataTransferObjects
+{
+    [DataContract]
+    public class CardDTO
+    {
+        [DataMember]
+        public int CardId { get; set; }
+        [DataMember]
+        public string FrontImage { get; set; }
+    }
+}
