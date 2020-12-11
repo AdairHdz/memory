60e5dddeda1f645d5d777ada7df3e488e3cd299d
Baltix2k <Baltix2k@users.noreply.github.com>
2020-12-10 21:51:26 -0600

    Implementacion de servicios IJoinGame y IWaitingRoom

---
+++ MemoryGameService/Utilities/GameSettings.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using MemoryGameService.Contracts;
+
+namespace MemoryGameService.Utilities
+{
+    class GameSettings
+    {
+        public int NumberOfPlayers { get; set; }
+
+        private Dictionary<IWaitingRoomServiceCallback, string> members;
+
+        public GameSettings()
+        {
+            members = new Dictionary<IWaitingRoomServiceCallback, string>();
+        }
+
+
+        public Dictionary<IWaitingRoomServiceCallback, string> getMembersDictionary()
+        {
+            return this.members;
+        }
+
+        public void addToMembersDictionary(IWaitingRoomServiceCallback connection, string username)
+        {
+            this.members.Add(connection, username);
+        }
+
+        public void setMembersDictionary(Dictionary<IWaitingRoomServiceCallback, string> newMembersDictionary)
+        {
+            this.members = newMembersDictionary;
+        }
+    }
+}
